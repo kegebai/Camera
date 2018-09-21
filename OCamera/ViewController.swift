d871116e6fe09c637d1458783fef4704cff0aec5
kegebai <kegebai@gmail.com>
2018-09-21 12:52:19 +0800

    Initial Commit

---
+++ OCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCamera
+//
+//  Created by kegebai on 2018/9/21.
+//  Copyright © 2018年 kegebai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
